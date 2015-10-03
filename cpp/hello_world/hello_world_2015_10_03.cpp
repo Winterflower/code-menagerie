ece01535c3ea16bd834a2fb7322ae5f1e6ad1ef2
Winterflower <camillamon@gmail.com>
2015-10-03 13:43:36 +0100

    C++ : Hello World

---
+++ cpp/hello_world/hello_world_2015_10_03.cpp
@@ -0,0 +1,10 @@
+//Hello World program on Oct 3rd, 2015
+
+#include <iostream> //preprocessor directive to include iostream libs
+
+int main(){
+    std::cout << "Hello, world on Oct 3rd, 2015" << std::endl;
+    return 0;
+}
+
+
